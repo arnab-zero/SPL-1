6b213060a7c9e85299d950d8d3eb3d4e0d8fda5f
Arnab Das Joy <darnabhere@gmail.com>
2023-02-06 23:10:34 +0600

    Code completed.

--- Maze Solving/Maze_Solving.cpp
+++ Maze Solving/Maze_Solving.cpp
@@ -17,6 +17,7 @@ vi dist(100);			// distance of each vertex from starting vertex
 vi previous(100);		// previous vertex of each vertex
 
 int nodeCounter = -1;			// starts nodes from 0
+int start, finish;		// start = start vertex in the graph, finish = destination in the graph		
 
 // BFS function for graph traversal
 void bfs(){
@@ -43,7 +44,7 @@ void bfs(){
 
 // main function
 int main(void){
-	int row, col;		
+	int row, col;
 	cout<<"Enter row and column count for the maze: ";
 	cin>>row>>col;
 
@@ -57,37 +58,49 @@ int main(void){
 	
 	// Only for the first row of the maze
 	for(j=0; j<col; ++j){
-		if(mazeLine[0][j] == '.'){
+		if(mazeLine[0][j] == '.' || mazeLine[0][j] == 'S' || mazeLine[0][j] == 'D'){
 			indexToNode[0][j] = ++nodeCounter;		// store the node to indexToNode table and name it
 			nodeToIndex.pb({0, j});				// store the index against the corresponding node
 
 			// for non-leftmost vertices connecting with the left vertex if exists
-			if(j && mazeLine[0][j-1] == '.'){		
+			if(j && (mazeLine[0][j-1] == '.' || mazeLine[0][j-1] == 'S' || mazeLine[0][j-1] == 'D')){		
 				mazeGraph[indexToNode[0][j-1]].pb(indexToNode[0][j]);		// push the edge to adjacency list
 				mazeGraph[indexToNode[0][j]].pb(indexToNode[0][j-1]);		// same to do undirected.
 			}
 		}
+
+		// save the vertex if it is start or end
+		if(mazeLine[0][j] == 'S')
+			start = nodeCounter;
+		else if(mazeLine[0][j] == 'E')
+			finish = nodeCounter;
 	}
 
 	// for next rows in the maze
 	for(i=1; i<row; ++i){
 		for(j=0; j<col; ++j){
-			if(mazeLine[i][j] == '.'){
+			if(mazeLine[i][j] == '.' || mazeLine[i][j] == 'S' || mazeLine[i][j] == 'D'){
 				indexToNode[i][j] = ++nodeCounter;		// store the node to indexToNode table and name it
 				nodeToIndex.pb({i, j});				// store the index against the corresponding node
 
 				// for non-leftmost vertices connecting with the left vertex if exists
-				if(j && mazeLine[i][j-1] == '.'){
+				if(j && (mazeLine[i][j-1] == '.' || mazeLine[i][j-1] == 'S' || mazeLine[i][j-1] == 'D')){
 					mazeGraph[indexToNode[i][j-1]].pb(indexToNode[i][j]);
 					mazeGraph[indexToNode[i][j]].pb(indexToNode[i][j-1]);
 				}
 
 				// creating edge with upper vertex if it is a '.'
-				if(mazeLine[i-1][j] == '.'){
+				if(mazeLine[i-1][j] == '.' || mazeLine[i-1][j] == 'S' || mazeLine[i-1][j] == 'D'){
 					mazeGraph[indexToNode[i-1][j]].pb(indexToNode[i][j]);
 					mazeGraph[indexToNode[i][j]].pb(indexToNode[i-1][j]);
 				}
 			}
+
+			// save the vertex if it is start or end
+			if(mazeLine[i][j] == 'S')
+				start = nodeCounter;
+			else if(mazeLine[i][j] == 'D')
+				finish = nodeCounter;
 		}
 	}
 
@@ -100,17 +113,46 @@ int main(void){
 	// 	}
 	// 	cout<<endl;
 	// }
+	//cout<<"Start: "<<start<<endl<<"Finish: "<<finish<<endl;
+
+	q.push(start);			// starting from the vertex 0
+	bfs();				//  BFS graph traversal on the graph found from the maze
+
+	// cout<<"After BFS: "<<endl;
+	// cout<<"Vertex"<<"\t"<<"Visited"<<" "<<"Distance"<<" "<<"Previous"<<endl;
+	// for(i=0; i<nodeCounter; ++i){
+	// 	cout<<i<<"\t"<<vis[i]<<" "<<dist[i]<<" "<<previous[i]<<endl;
+	// }
+	//cout<<finish<<"\t"<<vis[finish]<<" "<<dist[finish]<<" "<<previous[finish]<<endl;
 	
-	q.push(0);			// starting from the vertex 0
-	//  bfs() function call for graph traversal on the graph found from the maze
-	bfs();				
+	stack<int> s;		// stack to store the path from destination to current position.
 
-	cout<<"After BFS: "<<endl;
-	cout<<"Vertex"<<"\t"<<"Visited"<<" "<<"Distance"<<" "<<"Previous"<<endl;
-
-	for(i=0; i<nodeCounter; ++i){
-		cout<<i<<"\t"<<vis[i]<<" "<<dist[i]<<" "<<previous[i]<<endl;
+	int currentNode = finish;		// starting from destination.
+	s.push(currentNode);		// destination vertex pushed to the stack first.
+	while(currentNode != start){
+		currentNode = previous[currentNode];		// previous vertex would be new currentNode.
+		s.push(currentNode);		// current node pushed to the stack.
 	}
+
+	cout<<endl;
 	
+	if(vis[finish])
+		cout<<"Destination can be reached from the current position."<<endl;
+	else{
+		cout<<"There is no path to reach the destination."<<endl;
+		return 0;
+	} 
+
+	cout<<"Distance from the current position: "<<dist[finish]<<endl;
+
+	cout<<"Path from the current position to destination: ";
+	while(1){
+		cout<<s.top();
+		s.pop();
+		if(s.empty())
+			break;
+		cout<<" --> ";
+	}
+
 	return 0;
 }
