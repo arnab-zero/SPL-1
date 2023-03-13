a6e20e88a8ab702c0cd084eb8252955a280940c9
Arnab Das Joy <darnabhere@gmail.com>
2023-03-14 02:48:40 +0600

    Added source code implementation and sample input for Kruskal's algorithm.

---
+++ Resources/src/Kruskal_Algo_using_priority_queue.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define vi vector<int>
+#define pb push_back
+#define spc " "
+#define tab "\t"
+typedef long long int lli;
+
+
+// initializing parents
+void make_ds(int ds[], int v){
+    ds[v] = v;
+}
+
+// function for disjoint set find operation
+int ds_find(int ds[], int v){
+
+    if(ds[v]==v)
+        return v;
+    else 
+        return ds[v] = ds_find(ds,ds[v]);
+}
+
+// function for disjoint set union
+int ds_union(int ds[], int u, int v){
+
+    int parent_u = ds_find(ds,u), parent_v = ds_find(ds,v);
+
+    if(parent_u != parent_v){
+        ds[parent_v] = parent_u;
+    }
+    else    
+        return -1;
+}
+
+
+vector<pair<int,int>> mst;
+
+// Compare function for priority queue
+struct Compare {
+    bool operator() (const pair<int, pair<int,int>>& a, const pair<int, pair<int,int>>& b) {
+        return a.first > b.first; // Compare based on the first integer value
+    }
+};
+
+
+int main(void){
+
+    int vertices; 
+    cout<<"Enter the number of vertices: ";
+    cin>>vertices;
+
+    int edges;
+    cout<<"Enter the number of edges: ";
+    cin>>edges;
+
+    int ds[vertices+1];     // Disjoint set of the vertices
+
+    for(int i=1; i<vertices+1; ++i){
+        make_ds(ds,i);
+    }
+
+    cout<<endl<<"Enter the adjacency list: "<<endl;
+
+    priority_queue<pair<int, pair<int,int>>, vector<pair<int, pair<int,int>>>, Compare> pq;     // **********
+
+    for(int i=0; i<edges; ++i){
+        int u, v, w;
+        cin>>u>>v>>w;
+        pq.push({w,{u,v}});
+    }
+
+    vector<pair<int,int>> mst;
+    int minimum_path_weight = 0;
+
+    while(!pq.empty()){
+
+        auto element = pq.top();
+        pq.pop();
+
+        int u = element.second.first, v = element.second.second;
+
+        if(ds_union(ds,u,v) != -1){
+            mst.pb({u,v});
+            minimum_path_weight += element.first;       // Summing up the total weight
+        }
+    }
+
+    cout<<endl<<endl<<"Total minimum path weight: "<<minimum_path_weight<<endl;
+
+    for(auto i: mst){
+        cout<<(char)(i.first+'a'-1)<<spc<<(char)(i.second+'a'-1)<<endl;
+    }
+
+    return 0;
+}
